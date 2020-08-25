301c1885e9fe4b4ae0b80274f822ca461653aa08
ElinaZ88 <elina.zvargule@gmail.com>
2020-08-25 16:29:35 +0300

    All buttons connected

--- UIElements Layout/ViewController.swift
+++ UIElements Layout/ViewController.swift
@@ -43,17 +43,49 @@ class ViewController: UIViewController {
         }
         
     }
+    //MARK:- Logic Style to myLabel, Buttons and Image
+    
     func changeButtonImage(with image: String){
         leftButtonUp.setImage(UIImage(named: "home.png"), for: .normal)
         leftButtonUp.imageEdgeInsets = UIEdgeInsets(top: 8, left: 0, bottom: 10, right: 15)
         leftButtonUp.tintColor = .white
         self.view.backgroundColor = UIColor.black
     }
+    func changeButtonColor(){
+        rightButtonUp.backgroundColor = UIColor.red
+    }
+   
+    func changeTextlabel(){
+        self.myLabel.text = "You pressed the button"
+    }
+    
+    func changeImage(with image: String) {
+        mainImageView.image = UIImage(systemName: "sun.max", withConfiguration: UIImage.SymbolConfiguration(weight: .regular))
+        mainImageView.tintColor = .yellow
+    }
+    //MARK:- IBAction for three more buttons
     @IBAction func touchDragOutsideTapped(_ sender: Any) {
         print("touchDragOutsideTapped")
-        changeButtonImage(with: "home.png")    }
+        changeButtonImage(with: "home.png")
+        
+    }
+    
+    @IBAction func touchUpInsideTapped(_ sender: Any) {
+        print("touchUpInsideTapped")
+        changeButtonColor()
+        }
     
     
-
+    @IBAction func leftDownTapped(_ sender: Any) {
+        print("LeftButtonDownTapped")
+        changeTextlabel()
+        
+    }
+    
+    @IBAction func rightDownTapped(_ sender: Any) {
+        print("RightDownTapped")
+        changeImage(with: "sun.max")
+        
+    }
 }
 
