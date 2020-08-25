5b48530d268210636c9aad1638979b300e9f053a
ElinaZ88 <elina.zvargule@gmail.com>
2020-08-25 13:59:36 +0300

    Initial Commit

---
+++ UIElements Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIElements Layout
+//
+//  Created by elina.zvargule on 25/08/2020.
+//  Copyright © 2020 elina.zvargule. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
